d0e5b64c643a0031da7c5695b7631534cf3f8b83
ymekuria <ymekuria@gmail.com>
2015-12-28 20:38:11 -0800

    Added session cookies

--- shortly.js
+++ shortly.js
@@ -2,14 +2,14 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
-
-
+var cookieParser = require('cookie-parser');
 var db = require('./app/config');
 var Users = require('./app/collections/users');
 var User = require('./app/models/user');
 var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
+var session = require('express-session');
 
 var app = express();
 
@@ -18,14 +18,17 @@ app.set('view engine', 'ejs');
 app.use(partials());
 // Parse JSON (uniform resource locators)
 app.use(bodyParser.json());
+app.use(cookieParser());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
+
 app.get('/', 
 function(req, res) {
   res.render('index');
+  
 });
 
 app.get('/create', 
@@ -79,7 +82,7 @@ function(req, res) {
 app.post('/signup', 
 function(req, res) {
   var uri = req.body.url;
-  console.log('req.body', req.body.username);
+
 
   var data = {
     username: req.body.username,
@@ -88,6 +91,65 @@ function(req, res) {
    new User(data).save().then(function(model) {
     console.log('Everything is saved!');
     });
+
+
+});
+app.use(session({secret: 'ssshhhhh'}));
+app.post('/login', 
+  function(req, res) {
+    var uri = req.body.url;
+
+    var userN = req.body.username;
+    var password = req.body.password;
+    console.log('in login');
+    new User({username: userN})
+    .fetch()
+    .then(function(model) {
+      if(model.get('password') === password){
+        console.log("I already exist!");
+        /////////////ADDING SESSION//////////////
+        //app.use(session({secret: 'ssshhhhh'}));
+        
+        req.session.name = userN;
+        //res.redirect('/');
+
+
+
+
+
+
+        ///////////ADDING SESSION///////////////
+
+
+
+
+        ///////////////////////ADDING COOKIE HERE////////////////////////
+          // check if client sent cookie
+          // console.log(req.cookies);
+          // var cookie = req.cookies.cookieName;
+          // if (cookie === undefined)
+          // {
+          //   // no: set a new cookie
+          //   var randomNumber=Math.random().toString();
+          //   randomNumber=randomNumber.substring(2,randomNumber.length);
+          //   res.cookie('cookieName','1234', { maxAge: 900000, httpOnly: true });
+          //   console.log('res.cookie after assign');
+          //   console.log('cookie created successfully');
+          // } 
+          // else
+          // {
+          //   // yes, cookie was already present 
+          //   console.log('cookie exists', cookie);
+          // } 
+          // // //next(); // <-- important!
+      //////////////////////////END OF ADDING COOKIE///////////////////
+      } else {
+        console.log("sign up first");
+      //redirect to signup page
+    }
+  });
+
+   
 
 
 });
